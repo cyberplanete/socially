c05e058c16ced58a80bed76aa80e144ca8bff6b3
pascal Joret <pascal.joret@gmail.com>
2020-12-19 22:04:07 +0100

    Creation des controllers --Utilisateur connecté ou non ?

--- lib/controllers/main_app_Controller.dart
+++ lib/controllers/main_app_Controller.dart
@@ -4,13 +4,14 @@ class MainAppController extends StatefulWidget {
   _StateMainAppController createState() => _StateMainAppController();
 }
 
+///Cette page quand l'utilisateur est connecté
 class _StateMainAppController extends State<MainAppController> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
       child: Text(
-        'Another test',
+        'Connected',
         style: TextStyle(color: Colors.red),
       ),
     );
--- lib/controllers/signin_controller.dart
+++ lib/controllers/signin_controller.dart
@@ -4,13 +4,14 @@ class SigninController extends StatefulWidget {
   _SigninStateController createState() => _SigninStateController();
 }
 
+///Cette page quand l'utilisateur n'est pas connecté
 class _SigninStateController extends State<SigninController> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
       child: Text(
-        'test',
+        'Not Connected',
         style: TextStyle(color: Colors.green),
       ),
     );

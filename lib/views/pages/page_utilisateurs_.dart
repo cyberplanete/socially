cbc5da9d342f0a37b0cdfe73b9bff361757672fd
pascal <pascal.joret@gmail.com>
2021-06-10 21:08:16 +0200

    Contruction du streamBuilder pour afficher la liste des utilisateurs

--- lib/views/pages/page_utilisateurs_.dart
+++ lib/views/pages/page_utilisateurs_.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:socially/controllers/fireStoreController.dart';
 import 'package:socially/models/utilisateurs.dart';
 import 'package:socially/views/my_material.dart';
+import 'package:socially/views/my_widgets/my_loading_Center.dart';
 
 class PageUtilisateurs extends StatefulWidget {
   Utilisateur utilisateur;
@@ -12,10 +16,59 @@ class PageUtilisateurs extends StatefulWidget {
 class _PageUtilisateur extends State<PageUtilisateurs> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: MyText(
-        dataText: "Liste utilisateur",
-      ),
+    //Construction de la liste des utilisateurs avec un streamBuilder
+    //StreamBuilder écoute le Stream utilisateur, chaque fois que certaines données sortent de ce Stream, il se reconstruit automatiquement, appelant son callback builder.
+    return StreamBuilder<QuerySnapshot>(
+      builder:
+          (BuildContext buicontext, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasData) {
+          List<DocumentSnapshot> listOfUser = snapshot.data.docs;
+          return NestedScrollView(
+            headerSliverBuilder: (BuildContext buildContext, bool isScrolled) {
+              // Je retourne  un array
+              return [
+                SliverAppBar(
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                    title: MyText(
+                      dataText: "Liste d'utilisateurs",
+                      color: cBaseAccent,
+                    ),
+                    background: Image(
+                      image: cEventImage,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  expandedHeight: 150.0,
+                )
+              ];
+            },
+            //Construction de ma liste
+            body: ListView.builder(
+                itemCount: listOfUser.length,
+                itemBuilder: (BuildContext buildContext, int index) {
+                  Utilisateur utilisateur = Utilisateur(listOfUser[index]);
+                  return ListTile(
+                    leading: MyProfileImage(
+                        onPressed: null, urlString: utilisateur.imageUrl),
+                    title: MyText(
+                      dataText: "${utilisateur.nom} ${utilisateur.prenom}",
+                      color: cBaseAccent,
+                    ),
+                    trailing: TextButton(
+                        onPressed: null,
+                        child: MyText(
+                          dataText: "Suivre",
+                          color: cPointer,
+                        )),
+                  );
+                }),
+          );
+        } else {
+          return LoadingCenter();
+        }
+      },
+      stream: FireStoreController().fireStore_collectionOfUSers.snapshots(),
     );
   }
 }
